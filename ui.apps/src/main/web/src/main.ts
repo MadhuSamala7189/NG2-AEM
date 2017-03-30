4d84732c0c3c6471c25591929065d461040f5b43
madhukar.samala <madhukar.samala@gmail.com>
2017-03-29 20:53:54 -0500

    cleanup

--- ui.apps/src/main/web/src/main.ts
+++ ui.apps/src/main/web/src/main.ts
@@ -3,6 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { RootModule } from './app/root.module';
 
-console.info('angular2 instantiation');
+console.info('angular2 initialization');
 platformBrowserDynamic().bootstrapModule(AppModule);
 //platformBrowserDynamic().bootstrapModule(RootModule);
